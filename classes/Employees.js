4f7dd216ff96fc89c525ad29c63e4099eb7896c5
Kofi B <kofi.boateng@verizon.com>
2024-09-10 17:18:47 -0400

    updated employees class

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,7 +1,27 @@
 class Employees {
+    #salary
+    #isHired
+    constructor(name,title,salary,isHired = true){
+        this.name = name
+        this.title = title
+        this.#salary = salary
+        this.#isHired = isHired
+        let allEmployees = []
+    }
 
-
-
+    getSalary(){
+        return this.#salary
+    }
+    setSalary(amount){
+        this.#salary = amount
+    }
+    getStatus(){
+        return this.#isHired
+    }
+    setStatus(command){
+        if(command == 'hire'){this.#isHired = true}
+        if(command == 'fire'){this.#isHired = false}
+    }
     
 }
 
