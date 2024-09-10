4a2f5cb520cc1bb9ce945bc84346ecfad819a6db
Kofi B <kofi.boateng@verizon.com>
2024-09-10 17:49:37 -0400

    updated SE class

--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,6 +1,17 @@
-class SoftwareEngineer {
+const { Employees } = require("./Employees");
 
-
+class SoftwareEngineer extends Employees {
+#programmmingLanguages 
+constructor(name,title,salary,programmingLanguages = []){
+    super(name,title,salary)
+    this.#programmmingLanguages = programmingLanguages
+}
+getProgrammingLanguages(){
+    return this.#programmmingLanguages
+}
+setProgrammingLanguages(language){
+    this.#programmmingLanguages.push(language)
+}
 
     
 }
