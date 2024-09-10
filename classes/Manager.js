4e4718057e704bb337d0718f618492db2751df02
Kofi B <kofi.boateng@verizon.com>
2024-09-10 17:39:00 -0400

    updated manager class

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,5 +1,21 @@
-class Manager {
+const { Employees } = require("./Employees");
 
+
+class Manager extends Employees {
+#employeesManaged
+    constructor(name,title,salary,isHired,department,employeesManaged = []){
+        super(name,title,salary,isHired)
+        this.department = department
+        this.#employeesManaged = employeesManaged
+    }
+
+    getEmployeesManaged(){
+        return this.#employeesManaged
+    }
+
+    setEmployeesManaged(employee){
+        this.#employeesManaged = [employee]
+    }
 
 
     
