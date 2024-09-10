0bcfa2beff996494c874467bb3e081436b8567e1
Kofi B <kofi.boateng@verizon.com>
2024-09-10 18:28:45 -0400

    Updating Employee Class with Static Values

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,12 +1,13 @@
 class Employees {
     #salary
     #isHired
+    static #allEmployees = []
     constructor(name,title,salary,isHired = true){
         this.name = name
         this.title = title
         this.#salary = salary
         this.#isHired = isHired
-        let allEmployees = []
+        Employees.#allEmployees.push(this)
     }
 
     getSalary(){
@@ -22,7 +23,16 @@ class Employees {
         if(command == 'hire'){this.#isHired = true}
         if(command == 'fire'){this.#isHired = false}
     }
-    
+    static getEmployees(){
+        return Employees.#allEmployees
+    }
+    static getTotalPayroll(){
+        let total = 0
+        for (let i = 0; i < Employees.#allEmployees.length; i++){
+            total += Employees.#allEmployees[i].getSalary()
+        }
+        return total
+    }
 }
 
 module.exports = {
