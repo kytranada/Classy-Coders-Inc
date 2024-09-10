aba383f16b6c6757d2e245c5e8431bcc9260e980
Kofi B <kofi.boateng@verizon.com>
2024-09-10 17:55:36 -0400

    updated salesperson class

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,7 +1,18 @@
-class SalesPerson {
+const { Employees } = require("./Employees");
 
-
-
+class SalesPerson extends Employees{
+#totalSales
+constructor(name,title,salary,clients){
+    super(name,title,salary)
+    this.clients = clients
+    this.#totalSales = 0
+}
+getSalesNumber(){
+    return this.#totalSales
+}
+makeSale(amount){
+    this.#totalSales += amount
+}
     
 }
 
